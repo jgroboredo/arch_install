edae7b8f3036f43fa3adb916ec69ce99fbca649d
Your Name <you@example.com>
2021-05-12 00:19:41 +0100

    Fixed yay installer

--- yay_install.sh
+++ yay_install.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 
-echo 'Installing yay'
-install_yay
-
-
-echo 'Installing AUR packages'
-install_aur_packages
-
 install_yay() {
-    mkdir /foo
-    cd /foo
     git clone https://aur.archlinux.org/yay.git
     cd yay
     makepkg -si --noconfirm 
-
-    cd /
-    rm -rf /foo
+    cd ../
 }
 
 install_aur_packages() {
-    mkdir /foo
-    export TMPDIR=/foo
     yay -S --noconfirm acpi
     yay -S --noconfirm pamac-aur
     yay -S --noconfirm clipit
@@ -29,11 +16,12 @@ install_aur_packages() {
     yay -S --noconfirm polkit-gnome
     yay -S --noconfirm pa-applet-git
     yay -S pavucontrol
-    unset TMPDIR
-    rm -rf /foo
 }
 
+echo 'Installing yay'
+install_yay
 
 
-
+echo 'Installing AUR packages'
+install_aur_packages
 
