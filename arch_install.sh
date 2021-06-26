48bf5788fb7ff6490a17d7f68ca9b2690d32c81a
Gonçalo_Desktop <you@example.com>
2021-06-26 19:54:32 +0100

    Fixed $ signs in writting the mirrors. Splitted install_packages function

--- arch_install.sh
+++ arch_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 # Drive to install to.
 DRIVE='/dev/sda'
 
@@ -28,6 +30,7 @@ KEYMAP='pt-latin1'
 #swap_size(GB)
 SWAP_SIZE='2'
 
+####################################################
 # Choose your video driver
 # For Intel
 VIDEO_DRIVER="i915"
@@ -46,6 +49,21 @@ VIDEO_DRIVER="i915"
 
 # For amd
 #VIDEO_DRIVER="amd"
+
+###################################################
+
+#choose DE/WM
+#for i3
+DE="i3"
+
+#for plama
+#DE="plasma"
+if [ "$DE" = "i3" ]
+    then
+        source i3_packages.sh  
+fi
+###################################################
+
 
 if [ -d /sys/firmware/efi ]; then
     BIOS_TYPE="uefi"
@@ -91,6 +109,7 @@ setup() {
 
     echo 'Chrooting into installed system to continue setup...'
     cp $0 /mnt/setup.sh
+    cp $SCRIPT_DIR/${DE}_packages.sh /mnt/
     arch-chroot /mnt ./setup.sh chroot
 
     if [ -f /mnt/setup.sh ]
@@ -314,111 +333,6 @@ pacman_enable_repo() {
     sed -i "/\[$1\]/,/Include/"'s/^#//' /etc/pacman.conf
 }
 
-
-install_packages() {
-    local packages=''
-
-    # basic tools
-    packages+=' sudo xorg networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools linux-headers pacman-contrib' 
-    packages+=' bluez bluez-utils xdg-utils xdg-user-dirs git cmake expac'
-    packages+=' mlocate sshfs neofetch'
-    
-    #audio
-    packages+=' pulseaudio pavucontrol alsa-utils pulseaudio-bluetooth'
-
-    #video
-    packages+=' mpv'
-
-    #browser
-    packages+=' chromium firefox'
-
-    #grub related
-    packages+=' grub efibootmgr os-prober'
-
-    # Libreoffice
-    packages+=' libreoffice-fresh hyphen-en mythes-en'
-    
-    # i3
-    packages+=' i3 dmenu lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings picom guake pcmanfm xautolock flameshot'
-
-    # xfce4
-    packages+=' xfce4-terminal xfce4-power-manager xfce4-notifyd'
-    
-    # Fonts
-    packages+=' ttf-dejavu ttf-liberation noto-fonts otf-font-awesome'
-
-    # Themes, wallpapers and related apps
-    packages+=' papirus-icon-theme materia-gtk-theme lxappearance nitrogen archlinux-wallpaper arc-gtk-theme'
-    #packages+=' python-pywal'
-
-    #Automount usb devices
-    packages+=' gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb'
-
-    #ZSH related
-    packages+=' zsh zsh-theme-powerlevel10k zsh-syntax-highlighting zsh-completions'
-    
-    #Some tools (xrandr, net)
-    packages+=' arandr autorandr fuse2 htop inetutils net-tools netctl ntfs-3g pdf2svg tlp unzip cpupower ntp xarchiver p7zip'
-
-    #working tools
-    packages+=' zathura zathura-pdf-poppler texlive-most lyx jupyter jupyterlab python-numpy python-matplotlib python-scipy inkscape texmaker texlive-langgreek tmux viewnior rsync evince'
-
-    #dictionaries
-    packages+=' nuspell hspell libvoikko hunspell-en_US'
-
-    #thumbnails
-    packages+=' tumbler poppler-glib ffmpegthumbnailer freetype2 raw-thumbnailer libgsf libgepub'
-
-    # microcode for cpus
-    packages+=" ${ARCH_CPU_BRAND}-ucode"
-
-    # For laptops
-    packages+=' xf86-input-libinput'
-
-    # Gpu
-    if [ "$VIDEO_DRIVER" = "i915" ]
-    then
-        packages+=' xf86-video-intel libva-intel-driver mesa lib32-mesa vulkan-intel lib32-vulkan-intel'
-        packages+=' intel-compute-runtime intel-gpu-tools intel-media-driver'
-
-    elif [ "$VIDEO_DRIVER" = "intel-nvidia" ]
-    then
-        packages+=' xf86-video-intel libva-intel-driver mesa lib32-mesa vulkan-intel lib32-vulkan-intel'
-        packages+=' intel-compute-runtime intel-gpu-tools intel-media-driver'
-
-        packages+=' nvidia-dkms nvidia-settings nvidia-utils lib32-nvidia-utils libglvnd lib32-libglvnd'
-        packages+=' libvdpau lib32-libvdpau opencl-nvidia'
-    
-    elif [ "$VIDEO_DRIVER" = "amd-nvidia" ]
-    then
-        packages+=' xf86-video-amdgpu'
-
-        packages+=' nvidia-dkms nvidia-settings nvidia-utils lib32-nvidia-utils libglvnd lib32-libglvnd'
-        packages+=' libvdpau lib32-libvdpau opencl-nvidia'
-
-    elif [ "$VIDEO_DRIVER" = "intel-radeon" ]
-    then
-        packages+=' xf86-video-intel libva-intel-driver mesa lib32-mesa vulkan-intel lib32-vulkan-intel'
-        packages+=' intel-compute-runtime intel-gpu-tools intel-media-driver'
-
-        packages+=' mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver'
-        packages+=' mesa-vdpau lib32-mesa-vdpau opencl-mesa radeontop'
-    
-    elif [ "$VIDEO_DRIVER" = "amd-radeon" ]
-    then
-        packages+=' xf86-video-amdgpu'
-
-        packages+=' mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon libva-mesa-driver lib32-libva-mesa-driver'
-        packages+=' mesa-vdpau lib32-mesa-vdpau opencl-mesa radeontop'
-    
-    elif [ "$VIDEO_DRIVER" = "amd" ]
-    then
-        packages+=' xf86-video-amdgpu mesa lib32-mesa'
-
-    fi
-
-    pacman -Sy --noconfirm $packages
-}
 
 clean_packages() {
     yes | pacman -Scc
@@ -706,8 +620,8 @@ update_mirrorlist() {
     cat > /etc/pacman.d/mirrorlist <<EOF
 # mirrorlist for Portugal from arch-install
 
-Server = https://ftp.rnl.tecnico.ulisboa.pt/pub/archlinux/$repo/os/$arch
-Server = https://glua.ua.pt/pub/archlinux/$repo/os/$arch
+Server = https://ftp.rnl.tecnico.ulisboa.pt/pub/archlinux/\$repo/os/\$arch
+Server = https://glua.ua.pt/pub/archlinux/\$repo/os/\$arch
 EOF
 }
 
