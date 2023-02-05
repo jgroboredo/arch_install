dbc57d3930dedd8273e3f837291a664e041d66e6
goncalo-laptop <jgroboredo@gmail.com>
2023-02-05 00:43:45 +0000

    Fixing permissions

--- format_pi/verify.sh
+++ format_pi/verify.sh
@@ -2,11 +2,11 @@
 
 while IFS="" read -r p || [ -n "$p" ]
 do
-    if grep -q "$(echo $p | awk '{print $2}' )" permissions_etc_files.txt; then
+    file=$(echo $p | awk '{print $2}')
+    if grep -q "$file" permissions_etc_files.txt; then
         permission1="$(echo $p | awk '{print $1}')"
-        permission2="$(grep "$p" permissions_etc_files.txt | head -n1 | awk '{print $1}')"
+        permission2="$(grep "$file" permissions_etc_files.txt | head -n1 | awk '{print $1}')"
         if [[ "$permission1" != "$permission2" ]]; then
-            file=$(echo $p | awk '{print $2}')
             echo "Diferences in $file"
             sed -i "s#.*$file.*#$permission1 $file#" permissions_etc_files.txt
         fi
